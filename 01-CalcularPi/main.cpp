967d1be95cd8f354490d35b26f21eed5a9daff15
cgrinstein <135139499+cgrinstein@users.noreply.github.com>
2023-06-01 21:19:47 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+ 
+double PR=0,aux=0, i=0, pi=0, N=4, D=1;
+int alt=1;
 
+cout << "cant. de decimales de precision: "; cin >> PR;
+i=pow(10,PR);
+
+for (aux ; aux<i ; aux++);  {
+ pi=pi+alt*(N/D);
+   alt = alt*(-1);
+   D=D+2;
+}
+cout.precision(7);
+cout <<"pi: " << pi;
+
+return 0;
+}
